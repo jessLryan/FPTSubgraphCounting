538c1d66d048832f34f22f8316698271c9e15cd4
Jess <122939887+jessLryan@users.noreply.github.com>
2024-04-07 13:18:56 +0100

    happy with IntersectionGraphMerger

--- src/main/java/Algorithm/IntersectionGraphMerger.java
+++ src/main/java/Algorithm/IntersectionGraphMerger.java
@@ -15,7 +15,7 @@ public class IntersectionGraphMerger {
     private final IntersectionGraph graph2;
 
     public IntersectionGraphMerger(IntersectionGraph graph1, IntersectionGraph graph2) {
-        //graph1 should have fewest vertices for efficiency
+        //graph1 should have least vertices for efficiency
         if (graph1.order() < graph2.order()) {
             this.graph1 = graph1;
             this.graph2 = graph2;
@@ -34,58 +34,59 @@ public class IntersectionGraphMerger {
     }
 
     //a merged graph of graph1 and graph2 contains (a copy of)
-    //the vertices in both graphs, but at least one pair of vertices
-    //one from each graph must correspond to the same vertex in the new graph
+    //the vertices in both graphs such that at least one pair of vertices
+    //one from each graph must be 'merged' into to the same vertex in the new graph
     public List<IntersectionGraph> createMergedGraphs() {
         Map<Vertex, Vertex> mergedVertexMap = new HashMap<>();
-        return mergeVerticesRecursively(mergedVertexMap, new ArrayList<>(), graph2.getVertices(), 0);
+        return mergeVerticesRecursively(mergedVertexMap, graph1.getVertices(), graph2.getVertices(), 0);
     }
 
-    private List<IntersectionGraph> mergeVerticesRecursively(Map<Vertex, Vertex> mergedVertexMap, List<Vertex> unmergedVerticesGraph1, List<Vertex> unmergedVerticesGraph2, int nextVertexIndex) {
+    private List<IntersectionGraph> mergeVerticesRecursively(Map<Vertex, Vertex> mergedVertexMap, ArrayList<Vertex> unmergedVerticesGraph1, ArrayList<Vertex> unmergedVerticesGraph2, int nextVertexIndex) {
         //if unmergedVerticesGraph1 empty, we are done - check at least one pair are merged
         //and construct merged graph from map
-        if (nextVertexIndex == graph1.order()) {
-            //if no pairs of vertices are merged, don't create any graphs
+        if (nextVertexIndex == unmergedVerticesGraph1.size()) {
+            //if no pairs of vertices are merged, don't return any graphs
             //from map
             if (mergedVertexMap.isEmpty()) {
                 return List.of();
             } else {
-                return List.of(constructMergedGraphFromMap(mergedVertexMap, unmergedVerticesGraph1, unmergedVerticesGraph2));
+                return List.of(constructMergedGraph(mergedVertexMap, unmergedVerticesGraph1, unmergedVerticesGraph2));
             }
         }
 
         List<IntersectionGraph> mergedGraphs = new ArrayList<>();
         //option 1: merge next vertex in graph1 to each possible
         // remaining vertex in graph2
-        Vertex vertexGraph1 = graph1.getVertexAtIndex(nextVertexIndex);
+        Vertex vertexGraph1 = unmergedVerticesGraph1.get(nextVertexIndex);
         for (Vertex vertexGraph2 : unmergedVerticesGraph2) {
             Map<Vertex, Vertex> mergedVertexMapCopy = new HashMap<>(mergedVertexMap);
             mergedVertexMapCopy.put(vertexGraph1, vertexGraph2);
 
-            List<Vertex> unmergedVerticesGraph2Copy = new ArrayList<>(unmergedVerticesGraph2);
+            ArrayList<Vertex> unmergedVerticesGraph2Copy = new ArrayList<>(unmergedVerticesGraph2);
             unmergedVerticesGraph2Copy.remove(vertexGraph2);
 
-            mergedGraphs.addAll(mergeVerticesRecursively(mergedVertexMapCopy, unmergedVerticesGraph1, unmergedVerticesGraph2Copy, nextVertexIndex + 1));
+            ArrayList<Vertex> unmergedVerticesGraph1Copy = new ArrayList<>(unmergedVerticesGraph1);
+            unmergedVerticesGraph1Copy.remove(vertexGraph1);
+
+            mergedGraphs.addAll(mergeVerticesRecursively(mergedVertexMapCopy, unmergedVerticesGraph1Copy, unmergedVerticesGraph2Copy, nextVertexIndex));
         }
 
         //option 2: don't merge next vertex in graph1 to any vertex in graph2
-        List<Vertex> unmergedVerticesGraph1Copy = new ArrayList<>(unmergedVerticesGraph1);
-        unmergedVerticesGraph1Copy.add(vertexGraph1);
-        mergedGraphs.addAll(mergeVerticesRecursively(mergedVertexMap, unmergedVerticesGraph1Copy, unmergedVerticesGraph2, nextVertexIndex + 1));
+        mergedGraphs.addAll(mergeVerticesRecursively(mergedVertexMap, unmergedVerticesGraph1, unmergedVerticesGraph2, nextVertexIndex + 1));
 
         return mergedGraphs;
     }
 
 
-    private IntersectionGraph constructMergedGraphFromMap(Map<Vertex, Vertex> mergedVerticesMap, List<Vertex> unmergedVerticesGraph1, List<Vertex> unmergedVerticesGraph2) {
-
+    private IntersectionGraph constructMergedGraph(Map<Vertex, Vertex> mergedVerticesMap, List<Vertex> unmergedVerticesGraph1, List<Vertex> unmergedVerticesGraph2) {
         Map<Vertex, HashSet<Vertex>> correspondenceGraph1 = graph1.getCorrespondence();
         Map<Vertex, HashSet<Vertex>> correspondenceGraph2 = graph2.getCorrespondence();
         HashMap<Vertex, HashSet<Vertex>> newCorrespondence = new HashMap<>();
 
+        //note: new graph must contain new vertices as we change their neighbourhood
+
         //map every old vertex (from graph 1 or graph 2) to a new
         //vertex for the new merged graph
-        //also update the correpondence for each new vertex
         Map<Vertex, Vertex> oldToNewVertexMap = new HashMap<>();
 
         for (Vertex vertexGraph1 : unmergedVerticesGraph1) {
@@ -116,7 +117,10 @@ public class IntersectionGraphMerger {
 
         ArrayList<Vertex> newVertices = new ArrayList<>(oldToNewVertexMap.values().stream().distinct().toList());
         IntersectionGraph mergedGraph = new IntersectionGraph(newVertices, newCorrespondence);
-        mergedGraph.orderVerticesSoEachIsPrecededByNeighbour();;
+        //every intersection (pattern) graph is ordered in this way
+        //this is because we will use the brute force algorithm on these
+        //graphs and this makes it more efficient (see theoretical runtime details)
+        mergedGraph.orderVerticesSoEachIsPrecededByNeighbour();
         return mergedGraph;
     }
 
