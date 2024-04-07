6f838a1cb18e9f85c20a7aeecb4cd24ec24726e3
Jess <122939887+jessLryan@users.noreply.github.com>
2024-04-07 13:51:13 +0100

    happy with IntersectionSetFactory

--- src/main/java/Algorithm/IntersectionSetFactory.java
+++ src/main/java/Algorithm/IntersectionSetFactory.java
@@ -15,13 +15,15 @@ public class IntersectionSetFactory {
         return initialIntersectionSet;
     }
 
+    //the following method recursively determines the set of contained intersection sets for this set
+    //and of all of its contained intersection sets
     private static void createContainedIntersectionSetsOf(IntersectionSet intersectionSet) {
         int size = intersectionSet.size();
         ArrayList<IntersectionGraph> graphs = intersectionSet.getGraphs();
+        //for each pair of graphs, get all contained intersection sets in which this
+        // pair of graphs are merged together
         for (int graph1Index = 0; graph1Index < size - 1; graph1Index++) {
             for (int graph2Index = graph1Index + 1; graph2Index < size; graph2Index++) {
-                //get all contained intersection sets in which this
-                // pair of graphs are merged together
                 intersectionSet.addContainedIntersectionSets(createContainedIntersectionSetsWithGraphPairMerged(graphs, graph1Index, graph2Index));
             }
 
@@ -29,6 +31,8 @@ public class IntersectionSetFactory {
     }
 
     private static HashSet<IntersectionSet> createContainedIntersectionSetsWithGraphPairMerged(List<IntersectionGraph> graphs, int graph1Index, int graph2Index) {
+        //note that we use a set because we may encounter the same contained
+        //intersection set more than once
         HashSet<IntersectionSet> containedSets = new HashSet<>();
 
         IntersectionGraph graph1 = graphs.get(graph1Index);
@@ -37,18 +41,15 @@ public class IntersectionSetFactory {
         List<IntersectionGraph> mergedGraphs = graphMerger.createMergedGraphs();
 
         for (IntersectionGraph mergedGraph : mergedGraphs) {
-            ArrayList<IntersectionGraph> graphsCopy = new ArrayList<>(graphs);
-            graphsCopy.remove(graph1);
-            graphsCopy.remove(graph2);
-            graphsCopy.add(mergedGraph);
-            IntersectionSet containedIntersectionSet = new IntersectionSet(graphsCopy);
+            ArrayList<IntersectionGraph> containedSetGraphs = new ArrayList<>(graphs);
+            containedSetGraphs.remove(graph1);
+            containedSetGraphs.remove(graph2);
+            containedSetGraphs.add(mergedGraph);
+
+            IntersectionSet containedIntersectionSet = new IntersectionSet(containedSetGraphs);
+            //recurse to get all contained intersection sets of this set
             createContainedIntersectionSetsOf(containedIntersectionSet);
-            if (containedSets.contains(containedIntersectionSet)) {
-                System.out.println("was already there");
-            }
-            else {
-                containedSets.add(containedIntersectionSet);
-            }
+            containedSets.add(containedIntersectionSet);
             containedSets.addAll(containedIntersectionSet.getContainedIntersectionSets());
         }
         return containedSets;
